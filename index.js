df3b13d8435f5eb408f5ccf682115e682af9e6d0
Hernán González Moreno <hernangonzalezmoreno@gmail.com>
2021-06-30 11:38:13 -0300

    Deshabilitamos el uso de FindAndModify en moongose
    Lo deshabilitamos porque esta deprecado. Si no lo deshabilitamos, internamente lo usa las funciones findOneAndUpdate() y findOneAndDelete() del ORM

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var app = require( './app' );
 // Tomo el puerto si esta definido, o creo en el 3990
 var port = process.env.PORT || 3990;
 
+// Deshabilitamos el uso de FindAndModify en mongoose ya que esta deprecado
+mongoose.set( 'useFindAndModify', false );
+
 mongoose.Promise = global.Promise;
 mongoose.connect( 'mongodb://localhost:27017/api_rest_forum', { useNewUrlParser: true, useUnifiedTopology: true } )
         .then( () => {
