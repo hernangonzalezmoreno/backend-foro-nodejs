aee06b9d071c0d738802f3b7b9d7a024f46b6935
Hernán González Moreno <hernangonzalezmoreno@gmail.com>
2021-07-01 11:37:55 -0300

    TopicController@save: para guardar un nuevo topic

--- controllers/topic.js
+++ controllers/topic.js
@@ -1,11 +1,52 @@
 'use strict'
 
+var validator = require('validator');
+var Topic = require('../models/topic');
+
 var controller = {
-  test: function (req, res){
-    res.status(200).send({
-      message: 'Ruta de prueba topic'
+
+  save: function (req, res){
+
+    // Recogemos los parametros del post gracias a la libreria body-parser
+    let paramsPost = req.body;
+
+    let validate_title = paramsPost.title && !validator.isEmpty( paramsPost.title );
+    let validate_content = paramsPost.content && !validator.isEmpty( paramsPost.content );
+    let validate_lang = paramsPost.lang && !validator.isEmpty( paramsPost.lang );
+
+    if( !validate_title || !validate_content || !validate_lang ){
+      return res.status(400).send({
+        status: 'error',
+        message: 'Datos no validos.'
+      });
+    }
+
+    // Creamos el objeto Topic para guardar
+    let topic = new Topic();
+    topic.title = paramsPost.title;
+    topic.content = paramsPost.content;
+    topic.code = paramsPost.code;
+    topic.lang = paramsPost.lang;
+
+    // Guardamos el objeto
+    topic.save( (err, topicStored) => {
+
+      if( err || !topicStored){
+        return res.status(500).send({
+          status: 'error',
+          message: err? 'Error al guardar en la base de datos.' : 'Error al crear el topic en la base de datos.'
+        });
+      }
+
+      return res.status(200).send({
+        status: 'success',
+        topicStored
+      });
+
     });
+    
   }
+
 }
 
 module.exports = controller;
--- routes/topic.js
+++ routes/topic.js
@@ -6,6 +6,6 @@ var TopicController = require('../controllers/topic');
 var router = express.Router();
 var md_auth = require('../middlewares/authenticated.js');
 
-router.get( '/test', TopicController.test );
+router.post( '/save', md_auth.authenticated, TopicController.save );
 
 module.exports = router;
